41f6f7a3be33f599a2aa01ba8e3b6cff9bc19039
Lalise <lalisefufi5@gmail.com>
2026-05-15 13:56:58 +0300

    feat: add thematic analysis and run analysis

--- scripts/run_analysis.py
+++ scripts/run_analysis.py
@@ -1,19 +1,24 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pandas as pd
 
 from src.preprocessing import preprocess_dataset
 from src.sentiment_analysis import apply_sentiment
 from src.thematic_analysis import apply_theme_analysis
 
-# Load cleaned CSV from Task 1
+# Load dataset
 df = pd.read_csv("data/raw/bank_reviews_clean.csv")
 
-# Preprocessing
+# Preprocess
 df = preprocess_dataset(df)
 
-# Sentiment Analysis
+# Sentiment
 df = apply_sentiment(df)
 
-# Theme Analysis
+# Theme analysis
 df = apply_theme_analysis(df)
 
 # Save output
