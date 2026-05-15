0707635a2d472c52605305c7fbe9ce3ae1c20c68
Lalise <lalisefufi5@gmail.com>
2026-05-15 14:18:10 +0300

    feat: implement preprocessing analysis pipeline

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,37 +1,40 @@
 import pandas as pd
 import re
-import spacy
-from nltk.corpus import stopwords
 import nltk
 
-nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
-nlp = spacy.load("en_core_web_sm")
+nltk.download('stopwords')
+nltk.download('wordnet')
+
 stop_words = set(stopwords.words('english'))
 
+lemmatizer = WordNetLemmatizer()
+
 def clean_text(text):
+
     text = str(text).lower()
 
     # Remove URLs
     text = re.sub(r"http\S+", "", text)
 
-    # Remove punctuation
+    # Remove punctuation/numbers
     text = re.sub(r"[^a-zA-Z\s]", "", text)
 
-    # Tokenization + Lemmatization
-    doc = nlp(text)
+    tokens = text.split()
 
-    tokens = [
-        token.lemma_
-        for token in doc
-        if token.text not in stop_words
-        and not token.is_punct
-        and not token.is_space
+    cleaned_tokens = [
+        lemmatizer.lemmatize(word)
+        for word in tokens
+        if word not in stop_words
     ]
 
-    return " ".join(tokens)
+    return " ".join(cleaned_tokens)
+
 
 def preprocess_dataset(df):
+
     df = df.dropna(subset=["review", "rating"])
 
     df["clean_review"] = df["review"].apply(clean_text)
